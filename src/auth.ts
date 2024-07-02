75580009156b911109db7dd866f02751624b6647
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-07-02 16:01:33 -0400

    add prisma adapter to auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
+import { prisma } from '$lib/db'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import {SvelteKitAuth} from '@auth/sveltekit'
 import Google from '@auth/sveltekit/providers/google'
 
 export const {handle, signIn, signOut} = SvelteKitAuth({
-  providers: [Google]
+  providers: [Google],
+  adapter: PrismaAdapter(prisma)
 })
