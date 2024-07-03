3fdf7926e9d205395dd26b2723dabc89d0c4a818
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-07-03 14:20:32 -0400

    add feature to take in references to classes and class instances

---
+++ src/routes/api/getLessonContent/GetLessonContent.ts
@@ -0,0 +1,28 @@
+import { APIRoute, type RouteImplementation } from '$lib/abstract/APIRoute';
+import { jsonProps } from '$lib/generic/JsonProps';
+import { PrismaProps } from '$lib/generic/PrismaProps';
+import { MultiProp } from '$lib/helpers/MultiProp';
+import type { PrismaClient } from '@prisma/client';
+import { error } from '@sveltejs/kit';
+
+export class GetLessonContent implements RouteImplementation {
+	async call(props: { prisma: PrismaClient; lessonId: string }): Promise<object> {
+		let lesson = await props.prisma.lesson.findUnique({
+			where: {
+				id: Number(props.lessonId)
+			}
+		});
+
+		if (!lesson) throw error(404, 'Lesson not found');
+
+		return { textContent: lesson.textContent };
+	}
+}
+
+const GetLessonProps = jsonProps((json) => ({
+	lessonId: json.lessonId
+}));
+
+export const route = new APIRoute(
+  MultiProp.mergeProps([PrismaProps, GetLessonProps])
+)
