63b0e35a8146143fe30b50f8800ae087e74918a0
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-07-17 17:59:28 -0400

    add playground route

---
+++ src/routes/editor/[courseId]/[lessonId]/quiz/AnswerEditor.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let answerId: number;
+	export let answerText: string;
+	let hovering = false;
+</script>
+
+<div on:mouseover={() => (hovering = true)} on:mouseout={() => (hovering = false)}>
+  <div>
+    {answerText}
+    {#if hovering}
+      <button>Edit</button>
+    {/if}
+  </div>
+</div>
