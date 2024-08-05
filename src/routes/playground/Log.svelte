0a8c4d52168cf5bf731efcfffc55430af5f6f875
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-08-05 16:58:47 -0400

    add log component

---
+++ src/routes/playground/Log.svelte
@@ -0,0 +1,13 @@
+
+<script lang="ts">
+  let text = ""
+  export function appendToLog(text: string){
+    text += `${addLog}\n`
+  }
+
+  export function clearLog() {
+    text = ""
+  }
+</script>
+
+<textarea disabled>{text}</textarea>
