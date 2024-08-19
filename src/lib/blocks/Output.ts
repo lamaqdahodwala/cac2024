eff2d59c5817c29306a9f026a671149a15bad695
AeroKnight21 <joelraj3311@gmail.com>
2024-08-19 17:31:10 -0400

    add head and tail print blocks for df

--- src/lib/blocks/Output.ts
+++ src/lib/blocks/Output.ts
@@ -1,5 +1,8 @@
-import { CreateCategory, createCustomBlock } from '$lib/abstract/BlocklyInterface';
+import { createCustomBlock } from '$lib/abstract/BlocklyInterface';
 import { Order } from 'blockly/javascript';
+import { CreateCategory, type BlocklyJson, type BlockReturningValue, type CustomBlock } from '../abstract/BlocklyInterface';
+import type { Block } from 'blockly';
+import { JavascriptGenerator } from 'blockly/javascript';
 
 const printBlock = createCustomBlock(
 	{
@@ -19,10 +22,72 @@ const printBlock = createCustomBlock(
 	},
 	(block, generator) => {
 		const value_toprint = generator.valueToCode(block, 'toPrint', Order.ATOMIC);
-
 		const code = `appendToLog(${value_toprint})`;
 		return code;
 	}
 );
 
-export const OutputCategory = CreateCategory([printBlock], "Output");
+const printFirstRowsBlock = createCustomBlock(
+	{
+	  type: 'print_first_rows',
+	  tooltip: 'Print the first n rows of a dataframe',
+	  helpUrl: '',
+	  message0: 'In dataframe %1 print the first %2 rows',
+	  args0: [
+		{
+		  type: 'field_input',
+		  name: 'dataframe',
+		  text: 'df' // Default dataframe name
+		},
+		{
+		  type: 'field_number',
+		  name: 'rows',
+		  value: 5,
+		  min: 1,  
+		}
+	  ],
+	  previousStatement: null,
+	  nextStatement: null,
+	  colour: 135,
+	},
+	(block, generator) => {
+	  const dataframe = block.getFieldValue('dataframe');
+	  const rows = block.getFieldValue('rows');
+	  const code = `${dataframe}.head(${rows}).print()`;
+	  return code;
+	}
+  );
+  
+
+  const printLastRowsBlock = createCustomBlock(
+	{
+	  type: 'print_last_rows',
+	  tooltip: 'Print the last n rows of a dataframe',
+	  helpUrl: '',
+	  message0: 'In dataframe %1 print the last %2 rows',
+	  args0: [
+		{
+		  type: 'field_input',
+		  name: 'dataframe',
+		  text: 'df'
+		},
+		{
+		  type: 'field_number',
+		  name: 'rows',
+		  value: 5, 
+		  min: 1, 
+		}
+	  ],
+	  previousStatement: null,
+	  nextStatement: null,
+	  colour: 135,
+	},
+	(block, generator) => {
+	  const dataframe = block.getFieldValue('dataframe');
+	  const rows = block.getFieldValue('rows');
+	  const code = `${dataframe}.tail(${rows}).print()`;
+	  return code;
+	}
+  );
+
+export const OutputCategory = CreateCategory([printBlock, printFirstRowsBlock, printLastRowsBlock], "Output");
