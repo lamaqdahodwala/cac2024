a67f65c284d91c170872295febf4214899745a25
AeroKnight21 <joelraj3311@gmail.com>
2024-08-20 19:09:18 -0400

    add appending files

--- src/routes/playground/FileSystem.svelte
+++ src/routes/playground/FileSystem.svelte
@@ -1,31 +1,23 @@
 <script lang="ts">
   export function getFiles(){
-    return inputFile.files ? Array.from(inputFile.files) : [];
+    return choseFiles;
   }
 
   let file: File
   let inputFile: HTMLInputElement
-  let choseFiles: string[] = [];
+  let choseFiles: File[] = [];
 
-  export function getFileByName(fileName: string){
-    let files = inputFile.files
-
-    if (!files) return null
-
-    for (let index = 0; index < files.length; index++) {
-      const file = files.item(index);
-
-      if (file?.name === fileName) return file
+  export function getFileByName(fileName: string): File | null {
+    for (let file of choseFiles) {
+      if (file.name === fileName) return file;
     }
-
-    return null
+    return null;
   }
 
   function showFile() {
     if (inputFile.files && inputFile.files.length > 0) {
-      choseFiles = Array.from(inputFile.files).map(file => file.name);
-    } else {
-      choseFiles = ["No file chosen"];
+      const newFiles = Array.from(inputFile.files);
+      choseFiles = [...choseFiles, ...newFiles];
     }
   }
 
@@ -41,7 +33,7 @@
 
 <div>
   <label for="fileInput" class="button-style">Choose Files</label>
-  <span>{choseFiles.join(", ")}</span>
+  <span>{choseFiles.map(file => file.name).join(", ")}</span>
 </div>
 <input id="fileInput" bind:this={inputFile} type="file" on:change={showFile} multiple style="display: none;" />
 
