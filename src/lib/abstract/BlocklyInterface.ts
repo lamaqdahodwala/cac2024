c5c9955320ad4e2a5b2ae89e5f3fa67895000af5
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-08-23 18:01:12 -0400

    Revert 1 commits
    9efd39c 'add checkbox for boolean values'

--- src/lib/abstract/BlocklyInterface.ts
+++ src/lib/abstract/BlocklyInterface.ts
@@ -206,8 +206,8 @@ export type InputMapType = {
 		fields: {
 			fieldName: string;
 			opts: {
-				type: 'number' | 'text' | 'dropdown' | "boolean";
-				value?: number | string | boolean;
+				type: 'number' | 'text' | 'dropdown';
+				value?: number | string;
 				dropdownOptions?: {
 					internalName: string;
 					userText: string;
@@ -360,14 +360,12 @@ export function useInputMap(value: new () => InputMapMutator): MutatorMethods {
 							field = new Blockly.FieldDropdown(menu);
 							field.name = fieldConf.fieldName;
 						case 'number':
-							field = new Blockly.FieldNumber( Number(fieldConf.opts.value) );
+							field = new Blockly.FieldNumber(fieldConf.opts.value);
 							field.name = fieldConf.fieldName;
 							break;
 						case 'text':
 							field = new Blockly.FieldTextInput(String(fieldConf.opts.value));
 							field.name = fieldConf.fieldName;
-            case "boolean": 
-              field = new Blockly.FieldCheckbox(!!fieldConf.opts.value)
 					}
 
 					if (nextBlock === null) throw new Error();
