b2087689dba27d77afb53de19d69a6e6ec9325e8
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-08-27 17:30:03 -0400

    Stage files

---
+++ src/routes/editor/[courseId]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import LessonsSidebar from './[lessonId]/LessonsSidebar.svelte';
+	export let data: PageData;
+</script>
+
+<br><br>
+
+<div class="container">
+	<div class="columns">
+		<div class="is-2">
+			<LessonsSidebar courseId={data.courseId} />
+		</div>
+	</div>
+</div>
