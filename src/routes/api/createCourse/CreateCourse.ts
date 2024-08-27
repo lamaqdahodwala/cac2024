f1aa3dd2d6a784e434c00354bdf209b866fd5459
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-08-27 14:01:20 -0400

    revise the create course api route

--- src/routes/api/createCourse/CreateCourse.ts
+++ src/routes/api/createCourse/CreateCourse.ts
@@ -1,14 +1,21 @@
 import { APIRoute, type PropGetter, type RouteImplementation } from '$lib/abstract/APIRoute';
+import { AuthProps } from '$lib/generic/AuthProps';
 import { PrismaProps } from '$lib/generic/PrismaProps';
 import { MultiProp } from '$lib/helpers/MultiProp';
+import type { User } from '@auth/sveltekit';
 import type { PrismaClient } from '@prisma/client';
 import { error, type RequestEvent } from '@sveltejs/kit';
 
 export class CreateCourse implements RouteImplementation {
-	async call(props: { prisma: PrismaClient; courseTitle: string }): Promise<object> {
+	async call(props: { prisma: PrismaClient; courseTitle: string, user: User }): Promise<object> {
 		return await props.prisma.course.create({
 			data: {
-				title: props.courseTitle
+				title: props.courseTitle,
+        writer: {
+          connect: {
+            id: props.user.id
+          }
+        }
 			}
 		});
 	}
@@ -31,6 +38,6 @@ export class CreateCourseProps implements PropGetter {
 }
 
 export const route = new APIRoute(
-	MultiProp.merge([new PrismaProps(), new CreateCourseProps()]),
+	MultiProp.merge([new PrismaProps(), new CreateCourseProps(), new AuthProps()]),
 	new CreateCourse()
 );
---
+++ src/routes/editor/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+	import CreateCourseButton from "./CreateCourseButton.svelte";
+
+  export let data: PageData
+</script>
+
+{#each data.courses as course} 
+  <p>{JSON.stringify(course)}</p>
+{:else}
+  <p>No courses</p>
+{/each}
+
+<CreateCourseButton></CreateCourseButton>
