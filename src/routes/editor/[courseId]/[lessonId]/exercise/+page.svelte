d42785df451418af7840a97cc90ef5fb890cf2ef
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-09-24 17:28:20 -0400

    add exercise url

---
+++ src/routes/editor/[courseId]/[lessonId]/exercise/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let {
+		hasPrebuiltModelBlocks,
+		hasDataCleaningBlocks,
+		hasCustomModelBlocks,
+		hasBasicProgrammingBlocks,
+    hasTrainTestSplit, 
+    instructions, 
+    passRequirement,
+    testDataset, 
+    trainDataset
+	} = data.exerciseParams;
+</script>
+
+<h1 class="title">Edit exercise</h1>
+
+
