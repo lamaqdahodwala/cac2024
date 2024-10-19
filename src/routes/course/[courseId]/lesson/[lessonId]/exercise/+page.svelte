e578a11479706f9b53e9eb608c553ce565ae503f
Lamaq Dahodwala <lamaq091008@gmail.com>
2024-10-19 17:43:02 -0400

    add customizable category selector to course problem

--- src/routes/course/[courseId]/lesson/[lessonId]/exercise/+page.svelte
+++ src/routes/course/[courseId]/lesson/[lessonId]/exercise/+page.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
-	import Playground from "../../../../../playground/Playground.svelte";
-	import type { PageData } from "./$types";
+	import { ToolboxCreator } from '$lib/abstract/BlocklyInterface';
+	import { DataCleaningCategory } from '$lib/blocks/DataCleaning';
+	import { DataTransformationCategory } from '$lib/blocks/DataTransformation';
+	import { LayersCategory } from '$lib/blocks/Layers';
+	import { LoadingDataCategory } from '$lib/blocks/LoadingData';
+	import { ModelTrainingCategory } from '$lib/blocks/ModelTraining';
+	import { OptimizerCategory } from '$lib/blocks/Optimizers';
+	import { OutputCategory } from '$lib/blocks/Output';
+	import { addPrebuiltBlocks } from '$lib/blocks/PrebuiltBlocks';
+	import Playground from '../../../../../playground/Playground.svelte';
+	import type { PageData } from './$types';
 
-  export let data: PageData
+	export let data: PageData;
+
+	function getCategories(exerciseParams) {
+		let categories = [
+			OutputCategory,
+		];
+
+    if (exerciseParams.hasDataCleaningBlocks) {
+      categories.push(DataCleaningCategory, DataTransformationCategory, LoadingDataCategory)
+    }
+
+    if (exerciseParams.hasCustomModelBlocks) {
+      categories.push(ModelTrainingCategory, LayersCategory, OptimizerCategory)
+    }
+
+    return categories
+	}
+
 </script>
 
-{data}
 
-<!-- <Playground/> -->
+<Playground categories={getCategories(data.exerciseParams)}/>
